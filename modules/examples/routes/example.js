421f12734522ad37b981a454889b72884148435b
Matheus Fidelis <msfidelis@users.noreply.github.com>
2018-06-03 21:50:52 -0300

    Trocando a mensagem (#1)

--- modules/examples/routes/example.js
+++ modules/examples/routes/example.js
@@ -7,7 +7,7 @@ module.exports = [
         method: "GET",
         path: "/",
         handler: (req, res) => {
-            return res.response({foo: "bar"}).code(200)
+            return res.response({message: "the cake is a lie"}).code(200)
         }
     },
     {
